c921ac9e26900200145627bd626de016fe5b83a9
Lewis K <lewis.kwok.2023@bristol.ac.uk>
2024-04-23 16:07:41 +0100

    add tests to ParseDOTTests

--- cw-stag/src/test/java/edu/uob/ParseDOTTests.java
+++ cw-stag/src/test/java/edu/uob/ParseDOTTests.java
@@ -27,15 +27,15 @@ final class ParseDOTTests {
         File actionsFile = Paths.get("config" + File.separator + "basic-actions.xml").toAbsolutePath().toFile();
         server = new GameServer(entitiesFile, actionsFile);
         //Assert locations are loaded
-        assertEquals("cabin", server.getEntity("cabin").getName());
-        assertEquals("forest", server.getEntity("forest").getName());
-        assertEquals("storeroom", server.getEntity("storeroom").getName());
-        assertEquals("cellar", server.getEntity("cellar").getName());
+        assertEquals("cabin", server.getLocation("cabin").getName());
+        assertEquals("forest", server.getLocation("forest").getName());
+        assertEquals("storeroom", server.getLocation("storeroom").getName());
+        assertEquals("cellar", server.getLocation("cellar").getName());
         //Assert descriptions are loaded
-        assertEquals("A log cabin in the woods", server.getEntity("cabin").getDescription());
-        assertEquals("A dark forest", server.getEntity("forest").getDescription());
-        assertEquals("Storage for any entities not placed in the game", server.getEntity("storeroom").getDescription());
-        assertEquals("A dusty cellar", server.getEntity("cellar").getDescription());
+        assertEquals("A log cabin in the woods", server.getLocation("cabin").getDescription());
+        assertEquals("A dark forest", server.getLocation("forest").getDescription());
+        assertEquals("Storage for any entities not placed in the game", server.getLocation("storeroom").getDescription());
+        assertEquals("A dusty cellar", server.getLocation("cellar").getDescription());
         assertEquals("A log cabin in the woods", server.getLocation("cabin").getDescription());
         //Assert entities(artefact, furniture, character, paths) are loaded in locations
         assertEquals("A razor sharp axe", server.getLocation("cabin").getArtefact("axe").getDescription());
@@ -44,6 +44,32 @@ final class ParseDOTTests {
         assertEquals("A heavy wooden log", server.getLocation("storeroom").getArtefact("log").getDescription());
         assertEquals("Angry Elf", server.getLocation("cellar").getCharacter("elf").getDescription());
         assertEquals("Wooden trapdoor", server.getLocation("cabin").getFurniture("trapdoor").getDescription());
+        assertEquals("cabin", server.getStartingLocation().getName());
+    }
+
+    @Test
+    void parseExtendedDotFile(){
+        File entitiesFile = Paths.get("config" + File.separator + "extended-entities.dot").toAbsolutePath().toFile();
+        File actionsFile = Paths.get("config" + File.separator + "basic-actions.xml").toAbsolutePath().toFile();
+        server = new GameServer(entitiesFile, actionsFile);
+
+        //Assert entities of location are loaded
+        assertEquals("storeroom", server.getLocation("storeroom").getName());
+        assertEquals("A burly wood cutter", server.getLocation("storeroom").getCharacter("lumberjack").getDescription());
+        assertEquals("cabin", server.getStartingLocation().getName());
+    }
+    @Test
+    void parsePathsofDotFile(){
+        File entitiesFile = Paths.get("config" + File.separator + "extended-entities.dot").toAbsolutePath().toFile();
+        File actionsFile = Paths.get("config" + File.separator + "basic-actions.xml").toAbsolutePath().toFile();
+        server = new GameServer(entitiesFile, actionsFile);
+        //Assert paths are loaded
+        assertEquals("forest", server.getLocation("cabin").getPath("forest").getName());
+        assertEquals("cabin", server.getLocation("forest").getPath("cabin").getName());
+        assertEquals("cabin", server.getLocation("cellar").getPath("cabin").getName());
+        assertEquals("riverbank", server.getLocation("forest").getPath("riverbank").getName());
+        assertEquals("forest", server.getLocation("riverbank").getPath("forest").getName());
+        assertEquals("riverbank", server.getLocation("clearing").getPath("riverbank").getName());
     }
 
 
